61e346e872eb8ca3c36924fcd9b60df635250d0f
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-10-08 14:38:18 +0530

    Add files via upload

---
+++ COVIN.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class COVIN
+{
+    
+    public static void add_vaccine()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vaccine Name: ");
+        String name = sc.next();
+
+        System.out.println("Number of doses: ");
+        int doses = sc.nextInt();
+
+        System.out.println("Gap between doses: ");
+        int gap_doses = sc.nextInt();
+
+        System.out.println("Vaccine Name: " + name + ", Number of Doses: " + doses + ", Gap betweem Doses: " + gap_doses);
+        
+    }
+    public static void main(String[] args) 
+    {
+        //end
+	}
+
+}
