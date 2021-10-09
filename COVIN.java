eb8112aac1adeb7bbaad4e2a416d01cca808a07c
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-10-09 14:38:43 +0530

    Update COVIN.java

--- COVIN.java
+++ COVIN.java
@@ -1,25 +1,41 @@
 import java.util.*;
 
+import javax.swing.text.GapContent;
+
 public class COVIN
 {
     
-    public static void add_vaccine()
+    class add_vaccine
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Vaccine Name: ");
-        String name = sc.next();
+        void add__vaccine()
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Vaccine Name: ");
+            String name = sc.next();
+    
+            System.out.println("Number of doses: ");
+            int doses = sc.nextInt();
+    
+            int gap_doses;
+    
+            if (doses == 0 || doses == 1)
+            {
+                gap_doses = 0;
+                System.out.println("Vaccine Name: " + name + ", Number of Doses: " + doses + ", Gap betweem Doses: " + gap_doses); 
+            }
+    
+            else
+            {
+                System.out.println("Gap between doses: ");
+                gap_doses = sc.nextInt();
+                System.out.println("Vaccine Name: " + name + ", Number of Doses: " + doses + ", Gap betweem Doses: " + gap_doses);
+            }
+            
+            sc.close();
+        }
 
-        System.out.println("Number of doses: ");
-        int doses = sc.nextInt();
-
-        System.out.println("Gap between doses: ");
-        int gap_doses = sc.nextInt();
-
-        System.out.println("Vaccine Name: " + name + ", Number of Doses: " + doses + ", Gap betweem Doses: " + gap_doses);
-        
     }
-	
-    public static void  
+
     public static void main(String[] args) 
     {
         //end
